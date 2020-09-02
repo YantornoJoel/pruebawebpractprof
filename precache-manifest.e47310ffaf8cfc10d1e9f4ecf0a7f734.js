3b893ccac6907ff0cbbf8940db563eca5a82d06e
Joel <yantornojoel@gmail.com>
2020-09-02 14:50:06 -0300

    Updates

---
+++ precache-manifest.e47310ffaf8cfc10d1e9f4ecf0a7f734.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dff21f4f73f6fb3aa4718443533d17",
+    "url": "/pruebawebpractprof/index.html"
+  },
+  {
+    "revision": "014499b193206961f686",
+    "url": "/pruebawebpractprof/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "e1549c096aa08784bb49",
+    "url": "/pruebawebpractprof/static/css/main.9c73f142.chunk.css"
+  },
+  {
+    "revision": "014499b193206961f686",
+    "url": "/pruebawebpractprof/static/js/2.a5468227.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/pruebawebpractprof/static/js/2.a5468227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1549c096aa08784bb49",
+    "url": "/pruebawebpractprof/static/js/main.f89f8e22.chunk.js"
+  },
+  {
+    "revision": "f76224cd79f46f1d8c05",
+    "url": "/pruebawebpractprof/static/js/runtime-main.fcfc65c6.js"
+  },
+  {
+    "revision": "3ab9f344729baeb6a92c28167c81a08f",
+    "url": "/pruebawebpractprof/static/media/bosque.3ab9f344.jpg"
+  },
+  {
+    "revision": "99576dbdd09665fe34a92c2994ffee8a",
+    "url": "/pruebawebpractprof/static/media/iconedit.99576dbd.svg"
+  }
+]);
